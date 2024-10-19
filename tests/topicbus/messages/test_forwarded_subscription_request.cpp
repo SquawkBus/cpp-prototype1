88504fe2d0bd29c49f082b798380cade67781a7d
Rob Blackbourn <rob.blackbourn@gmail.com>
2024-10-19 09:11:03 +0100

    wip

---
+++ tests/topicbus/messages/test_forwarded_subscription_request.cpp
@@ -0,0 +1,26 @@
+#include <memory>
+
+#include "serialization/frame_buffer.hpp"
+#include "topicbus/messages/message.hpp"
+
+#define CATCH_CONFIG_MAIN
+#include "catch2/catch.hpp"
+
+using namespace squawkbus::serialization;
+using namespace squawkbus::topicbus::messages;
+
+TEST_CASE("smoke test")
+{
+    std::shared_ptr<Message> m0 = std::make_shared<ForwardedSubscriptionRequest>(
+        "host-1.example.com",
+        "tom.jones",
+        "client-1",
+        "TOPIC-1",
+        true
+    );
+    auto frame = m0->serialize();
+
+    auto m1 = Message::deserialize(frame);
+
+    REQUIRE(m0 == m1);
+}
---
+++ tests/topicbus/messages/test_notification_request.cpp
@@ -0,0 +1,23 @@
+#include <memory>
+
+#include "serialization/frame_buffer.hpp"
+#include "topicbus/messages/message.hpp"
+
+#define CATCH_CONFIG_MAIN
+#include "catch2/catch.hpp"
+
+using namespace squawkbus::serialization;
+using namespace squawkbus::topicbus::messages;
+
+TEST_CASE("smoke test")
+{
+    std::shared_ptr<Message> m0 = std::make_shared<NotificationRequest>(
+        ".*\\.L",
+        true
+    );
+    auto frame = m0->serialize();
+
+    auto m1 = Message::deserialize(frame);
+
+    REQUIRE(m0 == m1);
+}
