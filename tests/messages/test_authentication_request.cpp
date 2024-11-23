5939c36c2d377c51fae8b0e054d5fed1aaf54379
Rob Blackbourn <rob.blackbourn@gmail.com>
2024-11-23 10:48:06 +0000

    Use token

--- tests/messages/test_authentication_request.cpp
+++ tests/messages/test_authentication_request.cpp
@@ -2,18 +2,37 @@
 #include <vector>
 
 #include "messages/messages.hpp"
+#include "serialization/basic_token.hpp"
 
 #define CATCH_CONFIG_MAIN
 #include "catch2/catch.hpp"
 
 using squawkbus::messages::Message;
 using squawkbus::messages::AuthenticationRequest;
+using squawkbus::serialization::encode_basic_token;
 
-TEST_CASE("smoke test")
+using namespace std::string_literals;
+
+TEST_CASE("method none")
 {
     std::shared_ptr<Message> m0 = std::make_shared<AuthenticationRequest>(
-        "PLAIN",
-        std::vector {'f', 'r', 'e', 'd'}
+        "none",
+        ""
+    );
+    
+    auto frame = m0->serialize();
+
+    auto m1 = Message::deserialize(frame);
+
+    REQUIRE(m0 == m1);
+}
+
+TEST_CASE("method basic")
+{
+    auto token = encode_basic_token("aladdin", "opensesame");
+    std::shared_ptr<Message> m0 = std::make_shared<AuthenticationRequest>(
+        "basic",
+        token
     );
     
     auto frame = m0->serialize();
